209140cf1d3a700d746c6bdf3244610a86ffc330
Sandro Pischinger <sandro.pischinger@gmail.com>
2017-08-11 09:06:35 +0200

    added logo

--- IdeasyBackend/src/main/java/de/ideasy/backend/IdeasyBackendRuntime.java
+++ IdeasyBackend/src/main/java/de/ideasy/backend/IdeasyBackendRuntime.java
@@ -18,18 +18,28 @@ import java.util.Scanner;
  */
 public final class IdeasyBackendRuntime {
 
-    private final IDataManager dataManager;
+    private static final String LOGO = "\n_________ ______   _______  _______  _______          \n" +
+            "\\__   __/(  __  \\ (  ____ \\(  ___  )(  ____ \\|\\     /|\n" +
+            "   ) (   | (  \\  )| (    \\/| (   ) || (    \\/( \\   / )\n" +
+            "   | |   | |   ) || (__    | (___) || (_____  \\ (_) / \n" +
+            "   | |   | |   | ||  __)   |  ___  |(_____  )  \\   /  \n" +
+            "   | |   | |   ) || (      | (   ) |      ) |   ) (   \n" +
+            "___) (___| (__/  )| (____/\\| )   ( |/\\____) |   | |   \n" +
+            "\\_______/(______/ (_______/|/     \\|\\_______)   \\_/   \n" +
+            "                                                      ";
 
+    private final IDataManager dataManager;
     private final Logger logger;
 
     public IdeasyBackendRuntime() {
         this.logger = LoggerFactory.getLogger(IdeasyBackendRuntime.class);
+        this.logger.info(LOGO);
         this.getLogger().info("Initialising...");
         this.dataManager = new DataManager(new MySQLInfo(
                 "psandro.de",
-                "codeunddesign",
+                "",
                 "ideasy",
-                "codeunddesign"
+                ""
                 , 3306));
         this.getLogger().info("starting console...");
         new IdeasyConsole(new Scanner(System.in), this.dataManager);
