c19764233be6b83ccc5f9e7259737232c8c5133b
Julian <julian.schafer1@icloud.com>
2017-08-12 14:13:25 +0200

    rt

---
+++ frontend/ident-1.php
@@ -0,0 +1,60 @@
+<!doctype html>
+<html lang="de">
+<head>
+    <meta charset="utf-8"/>
+    <link rel="icon" type="image/png" href="icon--.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="icon--.png">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
+    <title>Ideasy Account Bestätigung</title>
+    <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport'/>
+    <meta name="viewport" content="width=device-width"/>
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet"/>
+    <link href="assets/css/paper-kit.css" rel="stylesheet"/>
+    <link href="assets/css/demo.css" rel="stylesheet"/>
+    <link href='http://fonts.googleapis.com/css?family=Montserrat:400,300,700' rel='stylesheet' type='text/css'>
+    <link href="http://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" rel="stylesheet">
+    <link href="assets/css/nucleo-icons.css" rel="stylesheet"/>
+    <link rel="stylesheet" href="assets/css/ident.css">
+    <style>
+        body {
+            margin-left: 40px;
+        }
+    </style>
+</head>
+<body>
+<h2>Fülle bitte noch die folgenden Felder aus</h2><br/>
+<div class="col-md-3">
+    <h3>Max Müller</h3><br/>
+    <div class="input-group form-group">
+        <span class="input-group-addon"><i class="nc-icon nc-email-85"></i></span>
+        <input type="email" id="email" name="email" class="form-control" required="" placeholder="Email"
+               onkeyup="myFunction()">
+    </div>
+    <div class="input-group form-group">
+        <span class="input-group-addon"><i class="nc-icon nc-email-85"></i></span>
+        <input type="email" id="emailBestaetigen" name="emailBestaetigen" class="form-control" required=""
+               placeholder="Email bestätigen">
+    </div>
+    <div class="input-group form-group">
+        <span class="input-group-addon"><i class="nc-icon nc-key-25"></i></span>
+        <input type="password" id="passwort" name="passwort" class="form-control" required="" placeholder="Passwort">
+    </div>
+    <div class="input-group form-group">
+        <span class="input-group-addon"><i class="nc-icon nc-key-25"></i></span>
+        <input type="password" id="passwortBestaetigen" name="passwortBestaetigen" class="form-control" required=""
+               placeholder="Passwort wiederholen">
+    </div>
+    <button type="button" class="btn btn-danger">Weiter</button>
+</div>
+</body>
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js"></script>
+<script src="assets/js/jquery-3.2.1.js" type="text/javascript"></script>
+<script src="assets/js/jquery-ui-1.12.1.custom.min.js" type="text/javascript"></script>
+<script src="assets/js/tether.min.js" type="text/javascript"></script>
+<script src="assets/js/bootstrap.min.js" type="text/javascript"></script>
+<script src="assets/js/bootstrap-switch.min.js"></script>
+<script src="assets/js/nouislider.js"></script>
+<script src="assets/js/moment.min.js"></script>
+<script src="assets/js/bootstrap-datetimepicker.min.js"></script>
+<script src="assets/js/paper-kit.js?v=2.0.0"></script>
+</html>
